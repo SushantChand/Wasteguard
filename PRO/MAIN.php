9817a77630dd6f4ee9ac6b812068680a0b653dc5
SushantChand <95869406+SushantChand@users.noreply.github.com>
2023-09-25 16:40:25 +0545

    Update MAIN.php

--- PRO/MAIN.php
+++ PRO/MAIN.php
@@ -13,6 +13,7 @@ define('LOCALHOST','localhost');
 <html lang="en">
  
 <head>
+ 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WasteGuard</title>
