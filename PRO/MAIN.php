e2afd8f6f7abec7dae8e1d54c7cf653861b1a17b
SushantChand <95869406+SushantChand@users.noreply.github.com>
2023-09-25 16:38:10 +0545

    Update MAIN.php

--- PRO/MAIN.php
+++ PRO/MAIN.php
@@ -7,6 +7,7 @@ define('LOCALHOST','localhost');
  $conn = mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD) or die(mysqli_error($conn));
  $db_select = mysqli_select_db($conn,DB_NAME) or die(mysqli_error($conn));
 ?>
+
 <!DOCTYPE html>
 
 <html lang="en">
